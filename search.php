8671782a913ea3e63b50167568e73c7ea2343a4c
pierrepatard <43677668+pierrepatard@users.noreply.github.com>
2018-10-05 16:29:43 +0200

    Add files via upload

---
+++ search.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nom Du Site</title>
+	<link rel="stylesheet" href="css/styleNav.css"/>
+	<link rel="stylesheet" href="css/styleHeader.css"/>
+	<link rel="stylesheet" href="css/styleSearche.css"/>
+</head>
+<body>
+
+	<?php include('header.php'); ?>
+	<?php include('nav.php'); ?>
+
+	<div id = "block_search">
+		<div>
+			<h1>Mots Cles : </h2>
+			<a href="product.php">
+				<div class = "item">
+					<div>
+						<h3>PC Bureau</h3>
+						<img src="pictures/computer.png">
+					</div>
+					<p>description du produit: magnifique pc fixe avec our plus ecran et autres peripheriques. Processeur i5, carte graphique GTX 1050, memoire vive: 8GO disc dure 1 To plus un SSD de 250 GO. ecran de 22 pouce avec resolution HD et temps de réponse 5ms.</p>
+					<div>
+						<p>PRIX : 1053 €</p>
+						<p>Stock : 10 </p>
+					</div>
+				</div>
+			</a>
+			<a href="product.php">
+				<div class = "item">
+					<div>
+						<h3>coffret gustatif</h3>
+						<img src="pictures/food.png">
+					</div>
+					<p>description du produit: magnifique coffret avec tasse a caffé, caffé en poudre et bien d'autre accessoirs.</p>
+					<div>
+						<p>PRIX : 10 €</p>
+						<p>Stock : 7 </p>
+					</div>
+				</div>
+			</a>
+			<a href="product.php">
+				<div class = "item">
+					<div>
+						<h3>manette de jeu</h3>
+						<img src="pictures/toy.png">
+					</div>
+					<p>description du produit: magnifique manette de jeu. convient aussi bien pour console que pour PC, mannette sans fils vendu avec le cable de charge.</p>
+					<div>
+						<p>PRIX : 53 €</p>
+						<p>Stock : 55 </p>
+					</div>
+				</div>
+			</a>
+			<a href="product.php">
+				<div class = "item">
+					<div>
+						<h3>casque moto !!!</h3>
+						<img src="pictures/sport.png">
+					</div>
+					<p>description du produit: magnifique casque de moto. Protection maximal grace au nouvelle aliage de polystirene et de fibre de carbonne pour encore plus de legerete et de confort. existe aussi avec écouteurs intégré!</p>
+					<div>
+						<p>PRIX : 153 €</p>
+						<p>Stock : 15 </p>
+					</div>
+				</div>
+			</a>
+		</div>
+	</div>
+</body>
+</html>
